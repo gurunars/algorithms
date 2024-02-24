6e3a28331b937a3539507b36d320298f33fdc0b0
Anton Berezin <gurunars@gmail.com>
2024-02-24 20:56:16 +0100

    ff

--- algorithms/ml/supervised/activation_functions.py
+++ algorithms/ml/supervised/activation_functions.py
@@ -1,40 +1,50 @@
+from dataclasses import dataclass
+
+from typing import List, Any
+
+from sympy import Symbol, Function, diff
+
+from extensions.dynamic_content import get_sympy_mathjax as jax
 
 
 def header(content: str) -> str:
     return f'<th style="font-size: 1.2em;" align="left">{content}</th>'
 
 
+z = Symbol("z")
+
+@dataclass
+class FunctionDef:
+    name: str
+    formula: Any
+
+functions: List[FunctionDef] = [
+    FunctionDef("Identity", z)
+]
+
+def row(func: FunctionDef):
+    return f"""
+    <tr>
+        <td>{func.name}</td>
+        <td>{jax(func.formula)}</td>
+        <td>{jax(diff(func.formula, z))}</td>
+        <td></td>
+    </tr>
+    """
+
+
 out = f"""
 <table class="docutils align-default">
     <tr>
         {header("Name")}
-        {header("g(z)")}
-        {header("g'(z)")}
+        {header(jax(Function("g")(z)))}
+        {header(jax(Function("g'")(z)))}
         {header("Plot")}
     </tr>
-    <tr>
-        <td>Identity</td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>RelU (Rectified Linear Unit)</td>
-        <td></td>
-        <td></td>
-        <td>dfefefefr 2r2343 gfbsdgbefdfv</td>
-    </tr>
-    <tr>
-        <td>Logistic (Sigmoid)</td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Logistic (Sigmoid)</td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
+    {
+        "\n".join(
+            row(it) for it in functions
+        )
+    }
 </table>
 """
