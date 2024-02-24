38faabad09fdeef4c90f62ce504d1213c3dbcb77
Anton Berezin <gurunars@gmail.com>
2024-02-24 21:38:11 +0100

    ff

--- algorithms/ml/supervised/activation_functions.py
+++ algorithms/ml/supervised/activation_functions.py
@@ -4,7 +4,7 @@ from typing import List, Any
 
 from sympy import (
     Symbol, Function, diff, exp,
-    Piecewise
+    Piecewise, log
 )
 
 from extensions.dynamic_content import get_sympy_mathjax as jax
@@ -15,6 +15,8 @@ def header(content: str) -> str:
 
 
 z = Symbol("z")
+
+alpha = Symbol(r"\alpha")
 
 @dataclass
 class FunctionDef:
@@ -40,6 +42,31 @@ functions: List[FunctionDef] = [
     FunctionDef(
         "TanH (Hyperbolic Tangent)",
         (exp(z) - exp(-z)) / (exp(z) + exp(-z))
+    ),
+    FunctionDef(
+        "Leaky (Parametric) RelU",
+        Piecewise(
+            (alpha * z, z < 0),
+            (z, z >= 0)
+        )
+    ),
+    FunctionDef(
+        "Binary step",
+        Piecewise(
+            (0, z < 0),
+            (1, z >= 0)
+        )
+    ),
+    FunctionDef(
+        "ELU (Exponential Linear Unit)",
+        Piecewise(
+            (alpha * (exp(z) - 1), z < 0),
+            (z, z >= 0)
+        )
+    ),
+    FunctionDef(
+        "SoftPlus",
+        log(1 + exp(z))
     )
 ]
 
